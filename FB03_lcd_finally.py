d910035c726479b86a54b5de9b07115b04bcb010
Fabio B <fabio.bertellotti@gmail.com>
2016-09-26 23:58:02 +0200

    Add files via upload

---
+++ FB03_lcd_finally.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#import pcd8544.lcd as lcd
+import lcdz as lcd
+import time, os, sys
+
+if not os.geteuid() == 0:
+    sys.exit('Script must be run as root')
+
+ON, OFF = [1, 0]
+
+try:
+  lcd.init()
+  lcd.cls()
+  lcd.backlight(ON)
+  lcd.centre_text(0,"Raspberry Pi")
+  while 1:
+     lcd.centre_text(2,time.strftime("%d %b %Y", time.localtime())) 
+     lcd.centre_text(4,time.strftime("%H:%M:%S", time.localtime()))
+     time.sleep(0.25)
+except KeyboardInterrupt:
+  pass
+finally:
+  lcd.cls()
+  lcd.backlight(OFF)
